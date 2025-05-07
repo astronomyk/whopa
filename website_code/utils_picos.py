4b91f762c04c10ef4862586c657b4e979748172e
Kieran Leschinski <kdleschinski@gmail.com>
2025-05-07 13:36:56 +0200

    added argv to util_picos

--- website_code/utils_picos.py
+++ website_code/utils_picos.py
@@ -85,6 +85,7 @@ def get_switch_gpio_status(port=SWITCH_PORT):
                     gpio_status[int(gpio)] = int(val)
                 except ValueError:
                     gpio_status[int(gpio)] = val  # in case of "ERR"
+        print(gpio_status)
         return gpio_status
 
     except subprocess.CalledProcessError as e:
